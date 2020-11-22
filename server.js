bb3d3b1066f15f8148f9b3459d0c41acbaa4d771
billdoss <abdouldosso78@gmail.com>
2020-11-22 21:34:33 +0000

    friendships services

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import User from './dbModel.js'
+import Friendships from './friendships/friendships.model.js'
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -12,16 +13,18 @@ app.use((req,res,next)=>{
     next() 
 })
 
-const connection_url ="mongodb+srv://wakandha:Wakandha@realmcluster.09rky.mongodb.net/wakandha?retryWrites=true&w=majority"
+const connection_url ="mongodb+srv://wakandha:Wakandha@realmcluster.09rky.mongodb.net/wakandha?retryWrites=true&w=majority";
 
 mongoose.connect(connection_url,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology:true, 
 })
+
 const Singin = async UserData =>{
  await User.create(UserData)
 }
+
 /* Pour rechercher les utilisateur qui sont dans la base de donnee */
 app.get("/userfind/",(req,res)=>{
    User.find((err,data)=>{
@@ -38,5 +41,65 @@ app.post('/userInscription/',(req,res)=>{
          Singin(req.body)
          res.send("oui")
 })
+
+
+/**
+ * FRIENDSHIPS SERVICE 
+ */
+
+/** FETCH ALL */
+app.get("/api/friendships",(req,res)=>{
+    Friendships.find((err,data)=>{
+         if(err){
+             res.status(500).send(err);
+         }else {
+             res.status(201).send(data);
+         }
+     })
+ })
+
+/** POST A FRIENDSHIP */
+app.post("/api/friendships", (req,res)=>{
+    Friendships.create({
+        user1: req.body.user1,
+        user2: req.body.user2,
+        createAt: { type: Date, default: Date.now }
+    }).then( friendships => res.json(friendships) );
+})
+
+/** DELETE BY user1 AND user2 */
+app.get("/api/friendships/:user1/:user2",(req,res)=>{
+    Friendships.deleteOne({ 'user1': req.params.user1, 'user2': req.params.user2 }, (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }else {
+            res.status(201).send(data);
+        }
+    });
+})
+
+/** SEARCH BY user1 AND user2 */
+app.get("/api/friendships/:user1/:user2",(req,res)=>{
+    Friendships.findOne({ 'user1': req.params.user1, 'user2': req.params.user2 }, (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }else {
+            res.status(201).send(data);
+        }
+      });
+ })
+
+
+/** SEARCH BY user */
+app.get("/api/friendships/:user",(req,res)=>{
+    Friendships.find({ 'user1': req.params.user }, (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }else {
+            res.status(201).send(data);
+        }
+    });
+})
+
 
 app.listen(port,()=>console.log(`Listen on localhost : ${port}`))
