320007c504769b9303bf8c9c6eef090a51fa31ad
billdoss <abdouldosso78@gmail.com>
2020-11-24 13:52:42 +0000

    notification in progress

--- routes/notifications.js
+++ routes/notifications.js
@@ -29,47 +29,47 @@ router.get("/",  (req,res, next)=>{
   
 })
 
-/** POST A FRIENDSHIP */
-router.post("/",   (req,res)=>{
-  Notification.create({
-      user1: req.body.user1,
-      user2: req.body.user2,
-      createAt: { type: Date, default: Date.now }
-  }).then( notification => res.json(notification) );
-})
+// /** POST A FRIENDSHIP */
+// router.post("/",   (req,res)=>{
+//   Notification.create({
+//       user1: req.body.user1,
+//       user2: req.body.user2,
+//       createAt: { type: Date, default: Date.now }
+//   }).then( notification => res.json(notification) );
+// })
 
-/** DELETE BY user1 AND user2 */
-router.get("/:user1/:user2", (req,res)=>{
-  Notification.deleteOne({ 'user1': req.params.user1, 'user2': req.params.user2 }, (err, data) => {
-      if(err){
-          res.status(500).send(err);
-      }else {
-          res.status(201).send(data);
-      }
-  });
-})
+// /** DELETE BY user1 AND user2 */
+// router.get("/:user1/:user2", (req,res)=>{
+//   Notification.deleteOne({ 'user1': req.params.user1, 'user2': req.params.user2 }, (err, data) => {
+//       if(err){
+//           res.status(500).send(err);
+//       }else {
+//           res.status(201).send(data);
+//       }
+//   });
+// })
 
-/** SEARCH BY user1 AND user2 */
-router.get("/:user1/:user2", (req,res)=>{
-  Notification.findOne({ 'user1': req.params.user1, 'user2': req.params.user2 }, (err, data) => {
-      if(err){
-          res.status(500).send(err);
-      }else {
-          res.status(201).send(data);
-      }
-    });
-})
+// /** SEARCH BY user1 AND user2 */
+// router.get("/:user1/:user2", (req,res)=>{
+//   Notification.findOne({ 'user1': req.params.user1, 'user2': req.params.user2 }, (err, data) => {
+//       if(err){
+//           res.status(500).send(err);
+//       }else {
+//           res.status(201).send(data);
+//       }
+//     });
+// })
 
 
-/** SEARCH BY user */
-router.get("/:user",  (req,res)=>{
-  Notification.find({ 'user1': req.params.user }, (err, data) => {
-      if(err){
-          res.status(500).send(err);
-      }else {
-          res.status(201).send(data);
-      }
-  });
-})
+// /** SEARCH BY user */
+// router.get("/:user",  (req,res)=>{
+//   Notification.find({ 'user1': req.params.user }, (err, data) => {
+//       if(err){
+//           res.status(500).send(err);
+//       }else {
+//           res.status(201).send(data);
+//       }
+//   });
+// })
 
 export default router
