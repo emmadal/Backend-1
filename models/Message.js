3f1775aa581b1ef0b30258016d6afdbc8b2caa4c
billdoss <abdouldosso78@gmail.com>
2020-11-24 13:44:22 +0000

    room and  message schema

---
+++ models/Message.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let Schema = mongoose.Schema;
+
+const MessageSchema = mongoose.Schema({ 
+  message_id: { type:mongoose.Schema.Types.ObjectId, required:true},
+  rooms_id: { type:mongoose.Schema.Types.ObjectId, required:true},
+  from: { type:mongoose.Schema.Types.ObjectId, ref:'User', required:true},
+  body: {
+      type: String, //{ text, gif, video, photo, pdf, ...}
+      content: [// contient les files id
+          { type: String },
+      ] 
+  },
+  replyTO: { type:mongoose.Schema.Types.ObjectId},
+  createDate: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+export default mongoose.model('message', MessageSchema)
---
+++ models/Room.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+let Schema = mongoose.Schema;
+
+const RoomSchema = mongoose.Schema({  
+  rooms_id: { type:mongoose.Schema.Types.ObjectId, required:true},
+  membre:[
+          { type:mongoose.Schema.Types.ObjectId, ref:'User', required:true},
+          { type:mongoose.Schema.Types.ObjectId, ref:'User', required:true},
+          { type:mongoose.Schema.Types.ObjectId, ref:'User'},
+      ],
+  createDate: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+export default mongoose.model('room', RoomSchema)
